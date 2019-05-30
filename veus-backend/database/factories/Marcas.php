ce88a2a70cedefc5cde55f202814095466f59b86
bruno dourado <brunomouradourado@gmail.com>
2019-05-30 16:22:08 -0300

    fixed errors

--- veus-backend/database/factories/Marcas.php
+++ veus-backend/database/factories/Marcas.php
@@ -4,6 +4,6 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Marcas::class, function (Faker $faker) {
     return [
-        'nome' => $faker->words,
+        'nome' => $faker->word,
     ];
 });
